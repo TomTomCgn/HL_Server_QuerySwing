1db38da97682a7b3e3b4d60824c744c66ced2dfd
TomTomCgn <thomas.lorenz.cgn@gmail.com>
2017-12-29 21:38:20 +0100

    Add PlaySound, Read Servers from servers.txt Fix 1. Build GUI and 2. Retrieve Server Info in a loop

--- HL_Server_QuerySwing/src/HL_Server_Query.java
+++ HL_Server_QuerySwing/src/HL_Server_Query.java
@@ -8,8 +8,13 @@ import javax.swing.JScrollPane;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -19,13 +24,29 @@ import javax.swing.JTable;
 public class HL_Server_Query {
 
 	private JFrame frame;
-	private JTable table;
+	private static JTable table;
 
 	/**
 	 * Launch the application.
+	 * @throws IOException 
+	 * @throws UnknownHostException 
+	 * @throws InterruptedException 
+	 * @throws LineUnavailableException 
+	 * @throws UnsupportedAudioFileException 
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException, UnsupportedAudioFileException, LineUnavailableException {
+		String result;
+		String[] parts;
+		String ia;
+		String port;
+		Integer  anzPlayer;
+		Integer merkerPlayer;
+		
+		HL_Server_Query window = new HL_Server_Query();
+		window.frame.setVisible(true);
+		
+		
+/*		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					HL_Server_Query window = new HL_Server_Query();
@@ -37,32 +58,16 @@ public class HL_Server_Query {
 					e.printStackTrace();
 				}
 			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 * @throws IOException 
-	 * @throws UnknownHostException 
-	 */
-	public HL_Server_Query() throws UnknownHostException, IOException {
-		String result;
-		String[] parts;
-		String ia;
-		String port;
-		Integer  anzPlayer;
-		Integer merkerPlayer;
-	
-		// GUI bauen
-		initialize();
+		});*/
 		
 		
+		// Ab hier Verarbeitung!?
 		// Server abfragen
         QueryClient client;
         client = new QueryClient();
         
         //ToDo Schleife alle 5 Sekunden
-        // while (true) {
+        while (true) {
 	        // durchlaufe Tabelle, besorge IP:Port und frage Server ab
 	        for (int i=0; i <   table.getRowCount(); i++) {
 	        	result = (String) table.getValueAt(i, 1);
@@ -72,24 +77,33 @@ public class HL_Server_Query {
 	        	ia = parts[0];
 	    		port = parts[1];
 	    		anzPlayer = client.getPlayer (client.sendReceiveQuery(ia,port));
-	    		System.out.println("Anzahl Spieler: " + anzPlayer);  
+//	    		System.out.println("Anzahl Spieler: " + anzPlayer);  
 	    		
 	    		// mit in Tabelle gespeichertem Player-Wert vergleichen
-	    		if (merkerPlayer < anzPlayer) {
-	    			// Unterschied? Dann Signalton
-
-	    			
+	    		if (merkerPlayer != anzPlayer) {  			
 	    			
 	    			// Tabelle mit aktueller Anzahl Spieler updaten
 	    			table.setValueAt(anzPlayer.toString(), i,2);
+	    			
+	    			// Unterschied? Dann Signalton
+	    			client.PlaySound();
+	    			
 	    		}
 	        }
-        try {
-			 Thread.sleep(500);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	        Thread.sleep(2000);
+        }
+		
+	}
+
+	/**
+	 * Create the application.
+	 * @throws IOException 
+	 * @throws UnknownHostException 
+	 */
+	public HL_Server_Query() throws UnknownHostException, IOException {
+	
+		// GUI bauen
+		initialize();
 	}
 		
 		
@@ -101,7 +115,7 @@ public class HL_Server_Query {
 	 * @throws UnknownHostException 
 	 */
 	private void initialize() throws UnknownHostException, IOException {
-		
+				
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 210);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -112,13 +126,45 @@ public class HL_Server_Query {
 		frame.getContentPane().add(lblHalfLifeServer, BorderLayout.NORTH);
 		
 		
-		//ToDo Aus Datei einlesen
-		String[][] rowData = {
-			      { "Cologne HL", "85.14.232.25:27015","0","0"  }, { "~ls", "178.161.179.214:27016","0","0" },
-//			      { "Spanien", "217" }, {"Türkei", "215"} ,{ "England", "214" },
-//			      { "Frankreich", "190" }, {"Griechenland", "185" },
-	//		      { "Deutschland", "180" }, {"Portugal", "170" }
-			    };
+		//TODO Aus Datei einlesen
+		String filename = "E:/servers.txt";
+		FileReader fr = new FileReader (filename);
+		BufferedReader br = new BufferedReader (fr);
+		
+		
+		// Datei hat 3 Einträge, mit ; getrennt
+		// Servername;IP:Port;checken J/N
+		
+		List<String> lines = new ArrayList<String>();
+		String line = null;
+		
+		while ((line = br.readLine()) != null) {
+			// gelesenen String spliten und zu rowData hinzufügen
+			//System.out.println( "Aus Datei gelesen:" + line);
+			lines.add(line);
+		}
+		
+        br.close();
+		
+		// Daten aus ArrayList nach roWData formatiert übergeben
+        String[][] rowData = new String[lines.size()][4];
+        
+        int i = 0;
+        
+        for (String s : lines ) {
+        	String[] tmp = s.split(";");
+        	rowData[i][0] = tmp[0];
+        	rowData[i][1] = tmp[1];
+        	rowData[i][2] = "0";
+        	rowData[i][3] = "0";
+        	i++;
+        }
+        
+        
+//		String[][] rowData = {
+//			      { "Cologne HL", "85.14.232.25:27015","0","0"  }, { "~ls", "178.161.179.214:27016","0","0" },
+//			      { "peet", "37.200.99.45:27020","0","0" }, 
+// 			    };
 
 			    String[] columnNames =  {
 			      "Servername", "IP:Port","Anzahl Player", "checken?"
